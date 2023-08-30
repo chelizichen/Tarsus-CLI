34f50fee4f195e7f22b3c8d010b7f0053014788f
chelizichen <1347290221@qq.com>
2023-08-30 19:47:21 +0800

    test:重新编译java结构体

--- bin/src/TaroCreateObj.js
+++ bin/src/TaroCreateObj.js
@@ -157,7 +157,7 @@ TaroCreateObject.Java.render = function (key, value, package) {
     let render = `
 package ${package};
 import com.tarsus.lib.lib_decorator.struct.TaroStruct;
-import com.tarsus.lib.main_control.load_server.TarsusJsonInf;
+import com.tarsus.lib.main_control.load_server.TarsusBodyABS;
 import com.tarsus.lib.main_control.load_server.impl.TarsusStream;
 import com.alibaba.fastjson.JSON;
 import java.util.HashMap;
@@ -168,7 +168,7 @@ import com.alibaba.fastjson.JSONObject;
 ${IMPORTS}
 
 @TaroStruct
-public class ${key} implements TarsusJsonInf{
+public class ${key} extends TarsusBodyABS{
   ${PARAMS}
 
   // ListConstructor
@@ -180,12 +180,6 @@ public class ${key} implements TarsusJsonInf{
   // NoArgsConstructor
   public ${key}(){
 
-  }
-  // toJson
-  @Override
-  public String json(){
-    Object o = JSONObject.toJSON(this);
-    return o.toString();
   }
 }
   `
